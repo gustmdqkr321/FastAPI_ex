26ff6264c95a7a0ce53e50e670cf8b852d74ce54
Park_SH <mbc3940@kookmin.ac.kr>
2024-01-01 22:18:28 +0900

    더하기 test

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/add")
+async def add_numbers(num1: int, num2: int):
+    total = sum(num1,num2)
+    return {"result": total}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def sum(a,b):
+    return a+b
